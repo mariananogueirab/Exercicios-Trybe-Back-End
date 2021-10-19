89b8543a044edf2b0cb64ed726d045dabaf74010
mariananbp <mariananbp@gmail.com>
2021-10-18 22:05:59 -0300

    Exercicio 1 - insert

---
+++ 19.4/Insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO Pixar.Movies(title, director, `year`, length_minutes)
+VALUES ('Monstros SA', 'Pete Docter', 2001, 92),
+('Procurando Nemo', 'John Lasseter', 2003, 107);
+
+SELECT * FROM Pixar.Movies;
