435f3b589b68c3b6cf7c3e12d2175c05f2482525
mariananbp <mariananbp@gmail.com>
2021-10-24 08:36:16 -0300

    insert e update

---
+++ 19.4/Insert 2.sql
@@ -0,0 +1,6 @@
+/* Exercício 2*/
+
+INSERT INTO Pixar.BoxOffice (movie_id, rating, domestic_sales, international_sales)
+VALUES (9, 6.8, 450000000, 370000000);
+
+SELECT * FROM Pixar.BoxOffice;
---
+++ 19.4/Update.sql
@@ -0,0 +1,7 @@
+/*Exercício 3 */
+
+UPDATE Pixar.Movies
+SET director = 'Andrew Staton'
+WHERE id = 9;
+
+SELECT * FROM Pixar.Movies;
